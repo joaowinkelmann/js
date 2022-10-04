5c5dabc999bd9828a9c62279013af52c8f24bf45
Joao Winkelmann <hmaster.studio@gmail.com>
2022-10-04 09:54:53 -0300

    final aula 11

---
+++ Área de Trabalho/cursos/js/D/aula11/testeexec.js
@@ -0,0 +1,16 @@
+// var booleana = true;
+// if(booleana){
+//     console.log("VERDADEIRO");
+// }
+// else{
+//     console.log("FALSO");
+// }
+
+var velocidade = 137;
+
+if (velocidade > 90){
+    console.log(`A velocidade do seu carro é de ${velocidade}km/h em uma rodovia de 90km/h, multado`);
+}
+else{
+    console.log(`O seu carro se encontra dentro da velocidade permitida`);
+}
