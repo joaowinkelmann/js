1b98305767fcc445d2aec86aac6c3e6475a4b210
Joao Winkelmann <hmaster.studio@gmail.com>
2022-10-05 09:19:06 -0300

    img.src ex02

--- Área de Trabalho/cursos/js/D/aula12ex2/script.js
+++ Área de Trabalho/cursos/js/D/aula12ex2/script.js
@@ -1,4 +1,5 @@
 function loader(){
+    var salve = window.document.querySelector('div#salve');
     var msg = window.document.getElementById('msg');
     var sorte = window.document.querySelector('div#sorte');
     var img = window.document.getElementById('imagem');
@@ -9,7 +10,20 @@ function loader(){
     msg.innerHTML = `<p>Agora são ${hora} horas e ${minutos} minutos.</p>`;
     sorte.innerHTML = `Seu número da sorte é igual a ${mili}.`;
 
+
+    //debug horas:
+    //hora = 20;
+
     if(hora > 5 && hora < 12){
-        img.innerHTML = `<img src="manha.jpg" alt="Bom dia">`;
+        salve.innerHTML = `Bom dia!`;
+        img.src = "manha.jpg";
+    }
+    else if (hora >= 12 && hora < 18){
+        salve.innerHTML = `Boa tarde!`;
+        img.src = "tarde.jpg";
+    }
+    else{
+        salve.innerHTML = "Boa noite!";
+        img.src = "noite.jpg";
     }
 }
