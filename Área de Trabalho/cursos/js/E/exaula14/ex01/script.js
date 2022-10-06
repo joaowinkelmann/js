91b1eb228da4f7baa6c32d08a595218569c39ee7
Joao Winkelmann <hmaster.studio@gmail.com>
2022-10-06 11:20:47 -0300

    quase final exercicio 14

---
+++ Área de Trabalho/cursos/js/E/exaula14/ex01/script.js
@@ -0,0 +1,26 @@
+var inicio = document.querySelector('input#inicio');
+var max = document.querySelector('input#max');
+var incr = document.querySelector('input#incr');
+
+var result = document.querySelector('div#resultado');
+
+
+function contar(){
+    result.innerHTML = null;
+
+    if(inicio.value.length == 0 || max.value.length == 0 || incr.value.length == 0){
+        window.alert("Dados formatados de maneira equivocada");
+    }
+
+    var val_i = Number(inicio.value);
+    var val_m = Number(max.value);
+    var val_s = Number(incr.value);
+
+    // for(i=0;i<10;i++){
+    //     result.innerHTML += `${i}`;  
+    // }
+
+    for(i = val_i; i <= val_m; i += val_s ){
+        result.innerHTML += `${i} `;
+    }
+}                   
