bde793eb54e71ecaf84d0a2dcd220a31c8cf8517
Joao Winkelmann <hmaster.studio@gmail.com>
2022-10-07 08:22:00 -0300

    final ex 02 aula 14

--- Área de Trabalho/cursos/js/E/exaula14/ex02/script.js
+++ Área de Trabalho/cursos/js/E/exaula14/ex02/script.js
@@ -1,10 +1,14 @@
 var result = document.querySelector('div#resultado');
+var tab = document.getElementById('seltab');
 function tabuada(){
     let base = (document.querySelector('input#base')).value;
     
-    result.innerHTML = null;
+    tab.innerHTML = null;
     for(i=1; i <= 10; i++){
-        let aux = i * base;
-        result.innerHTML += `${i} x ${base} = ${aux}<br>`;
+        let item = document.createElement('option');
+        item.text = `${base} x ${i} = ${i*base}`;
+        item.value = `tab${i}`;
+        tab.appendChild(item);
+        // result.innerHTML += `${i} x ${base} = ${aux}<br>`;
     }
 }
